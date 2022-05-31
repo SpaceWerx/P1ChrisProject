c0f0b915f0e03f0b92ac589d28fa264bf45166af
christine nwonuala <chrisnwogu5@gmail.com>
2022-05-30 22:33:31 -0400

    update

--- src/main/java/com/revature/P1ChrisProjectApplication.java
+++ src/main/java/com/revature/P1ChrisProjectApplication.java
@@ -3,11 +3,19 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import io.javalin.Javalin;
+
+//@SpringBootApplication
 public class P1ChrisProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(P1ChrisProjectApplication.class, args);
-	}
+		//SpringApplication.run(P1ChrisProjectApplication.class, args)
+		
 
+	Javalin app = Javalin.create(
+				config -> {
+					config.enableCorsForAllOrigins();
+				}
+			).start(3000);
+	}
 }
---
+++ src/main/java/com/revature/service/UserServicex.java
@@ -0,0 +1,59 @@
+package com.revature.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.revature.models.Role;
+import com.revature.models.User;
+import com.revature.repository.UserRepositoryDAO;
+
+
+@Service("userService")
+public class UserServicex
+{
+	UserRepositoryDAO userRepo;
+		
+    public User getUserByName(String userName)
+   	{
+		return userRepo.findByUserName(userName);
+    }
+	
+	
+    public User getUserByID(int userId)
+   	{
+		return userRepo.findByUserId(userId);
+    }
+	
+	
+    public List<User> getAllUsers()
+   	{
+		return userRepo.findAll();
+    }
+	
+
+    public List<User> getUsersByRole(Role userRole)
+   	{
+		return userRepo.findUsersByRole(userRole);
+    }
+	
+    
+    public boolean checkUserExists(int userId)
+   	{
+		User user = userRepo.findByUserId(userId);
+    	if(user!=null)
+    		return true;
+    	else
+    		return false;
+    }
+    
+    public User create(User submittedUser)
+   	{
+    	//ID is Auto increment, So no need to set ID.
+    	int userId = userRepo.createUser(submittedUser);
+    	return userRepo.findByUserId(userId);
+    	
+    }
+    
+	
+}
